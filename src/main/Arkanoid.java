b965a4d87c59473dc5ebf489b3d25921cff91081
Deety-anh <24021369@vnu.edu.vn>
2025-10-28 22:50:02 +0700

    Fix collision bugs
    Change to vector move

--- src/main/Arkanoid.java
+++ src/main/Arkanoid.java
@@ -1,3 +1,6 @@
+package main;
+
+import controller.dat.dat;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -5,12 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Arkanoid extends Application {
+public class Arkanoid extends Application implements dat {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Arkanoid.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Arkanoid.class.getResource("/controller/fxml/Ingame-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), width, height);
+        stage.setTitle("Simple Arkanoid Game");
         stage.setScene(scene);
         stage.show();
     }
