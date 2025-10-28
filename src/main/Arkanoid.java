1ea8c6d02b629554f88ff340fb26027473357b90
Deety-anh <24021369@vnu.edu.vn>
2025-10-28 23:33:24 +0700

    Update

---
+++ src/main/Arkanoid.java
@@ -0,0 +1,19 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Arkanoid extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Arkanoid.class.getResource("/controller/fxml/Ingame-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Simple Arkanoid Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
