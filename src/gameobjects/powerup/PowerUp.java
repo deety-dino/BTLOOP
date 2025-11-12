ee52de4e510344018f93e5130106a699786ae470
Phạm Minh Đức <24021418@vnu.edu.vn>
2025-11-12 18:43:32 +0700

    add them power-up

--- src/gameobjects/powerup/PowerUp.java
+++ src/gameobjects/powerup/PowerUp.java
@@ -1,5 +1,4 @@
 package gameobjects.powerup;
-
 import gameobjects.Ball.Ball;
 import gameobjects.Controller.BallController;
 import gameobjects.Controller.objectInfo;
@@ -8,7 +7,8 @@ import gameobjects.paddle.Paddle;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -21,39 +21,37 @@ public class PowerUp extends GameObject {
         WIDE_PADDLE,
         SPEED_BALL,
         MULTI_BALL,
-        LASER_PADDLE  // New type: adds shooting capability
+        LASER_PADDLE
     }
 
     public PowerUp(double x, double y, PowerUpType type) {
         super(x, y, 20, 20);
         this.type = type;
-        shape = new Rectangle(20, 20);
 
-        // Enhanced visual feedback for different types
-        Color powerUpColor;
-        switch (type) {
-            case WIDE_PADDLE:
-                powerUpColor = Color.YELLOW;
-                break;
-            case SPEED_BALL:
-                powerUpColor = Color.RED;
-                break;
-            case MULTI_BALL:
-                powerUpColor = Color.GREEN;
-                break;
-            case LASER_PADDLE:
-                powerUpColor = Color.PURPLE;
-                break;
-            default:
-                powerUpColor = Color.WHITE;
+        // Try to load power-up image based on type
+        String imagePath = getPowerUpImagePath(type);
+        Color fallbackColor = getPowerUpColor(type);
+
+        try {
+            Image powerUpImage = new Image(getClass().getResourceAsStream(imagePath));
+            ImageView imageView = new ImageView(powerUpImage);
+            imageView.setFitWidth(20);
+            imageView.setFitHeight(20);
+            imageView.setPreserveRatio(false);
+            shape = imageView;
+        } catch (Exception e) {
+            System.out.println("Power-up image not found: " + imagePath + ", using fallback");
+            e.printStackTrace();
+
+            // Fallback to colored rectangle
+            Rectangle rect = new Rectangle(20, 20);
+            rect.setFill(fallbackColor);
+            rect.setArcWidth(10);
+            rect.setArcHeight(10);
+            rect.setStroke(Color.WHITE);
+            rect.setStrokeWidth(2);
+            shape = rect;
         }
-
-        Rectangle rect = (Rectangle) shape;
-        rect.setFill(powerUpColor);
-        rect.setArcWidth(10);
-        rect.setArcHeight(10);
-        rect.setStroke(Color.WHITE);
-        rect.setStrokeWidth(2);
 
         // Position the shape according to the GameObject position
         shape.setLayoutX(position.getX());
@@ -61,6 +59,36 @@ public class PowerUp extends GameObject {
 
         // Randomize initial fall speed slightly
         fallSpeed = objectInfo.BASE_FALL_SPEED * (0.8 + random.nextDouble() * 0.4);
+    }
+
+    private String getPowerUpImagePath(PowerUpType type) {
+        switch (type) {
+            case WIDE_PADDLE:
+                return "/gfx/power-up/PaddleSize.png";
+            case SPEED_BALL:
+                return "/gfx/power-up/Speed.png";
+            case MULTI_BALL:
+                return "/gfx/power-up/double.png";  // You'll need to add this image
+            case LASER_PADDLE:
+                return "/gfx/power-up/bullet.png";  // You'll need to add this image
+            default:
+                return "/gfx/power-up/PaddleSize.png";
+        }
+    }
+
+    private Color getPowerUpColor(PowerUpType type) {
+        switch (type) {
+            case WIDE_PADDLE:
+                return Color.YELLOW;
+            case SPEED_BALL:
+                return Color.RED;
+            case MULTI_BALL:
+                return Color.GREEN;
+            case LASER_PADDLE:
+                return Color.PURPLE;
+            default:
+                return Color.WHITE;
+        }
     }
 
     @Override
@@ -72,13 +100,23 @@ public class PowerUp extends GameObject {
 
     //Wide Paddle powerup
     public static void widePaddle_Activation(Paddle paddle) {
-        Rectangle paddleShape = (Rectangle) paddle.getNode();
-        paddleShape.setWidth(objectInfo.defaultPaddleSize * 1.5);
+        if (paddle.getNode() instanceof Rectangle) {
+            Rectangle paddleShape = (Rectangle) paddle.getNode();
+            paddleShape.setWidth(objectInfo.defaultPaddleSize * 1.5);
+        } else if (paddle.getNode() instanceof ImageView) {
+            ImageView paddleImage = (ImageView) paddle.getNode();
+            paddleImage.setFitWidth(objectInfo.defaultPaddleSize * 1.5);
+        }
     }
 
     public static void widePaddle_Deactivation(Paddle paddle) {
-        Rectangle paddleShape = (Rectangle) paddle.getNode();
-        paddleShape.setWidth(objectInfo.defaultPaddleSize * 1.0);
+        if (paddle.getNode() instanceof Rectangle) {
+            Rectangle paddleShape = (Rectangle) paddle.getNode();
+            paddleShape.setWidth(objectInfo.defaultPaddleSize * 1.0);
+        } else if (paddle.getNode() instanceof ImageView) {
+            ImageView paddleImage = (ImageView) paddle.getNode();
+            paddleImage.setFitWidth(objectInfo.defaultPaddleSize * 1.0);
+        }
     }
 
     //Speed Ball powerup
@@ -112,9 +150,7 @@ public class PowerUp extends GameObject {
 
     //Laser Paddle powerup
     public static void laserPaddle_Activation() {
-
     }
-
 
     public PowerUpType getType() {
         return type;
